dcf84fbc01d1050344e71e395c27d67e9929804c
cara ottmar <cara.ottmar@gmail.com>
2018-03-27 10:53:54 -0700

    updated server code

--- server.js
+++ server.js
@@ -17,7 +17,8 @@ const CLIENT_URL = process.env.CLIENT_URL;
 // const client = new pg.Client(process.env.DATABASE_URL);
 // client.connect();
 // client.on('error', err => console.error(err));
-
+// COMMENT: application middleware
+app.use(cors());
 //API endpoints
 
 app.get('/api/v1/books', (req, res) => res.send('It lives!'));
